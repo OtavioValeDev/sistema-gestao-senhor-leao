73206206a2a2d5f81699dc529f11995033770338
Matheus Bonato <bqnato@gmail.com>
2025-12-06 10:27:30 -0300

    Criado Controller do Funcionario

---
+++ src/main/java/com/example/projeto_test/funcionario/controller/FuncionarioController.java
@@ -0,0 +1,44 @@
+package com.example.projeto_test.funcionario.controller;
+
+import com.example.projeto_test.funcionario.model.Funcionario;
+import com.example.projeto_test.funcionario.service.FuncionarioService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/employee")
+public class FuncionarioController {
+
+    private final FuncionarioService funcionarioService;
+
+
+    @PostMapping("/create")
+    public ResponseEntity<String> adicionarFuncionario(@RequestBody Funcionario funcionario){
+        funcionarioService.create(funcionario);
+        return ResponseEntity.ok().body(funcionario + "Funcionario criado!");
+    }
+
+    @GetMapping("/{id}")
+    public Funcionario findFuncionarioById(@PathVariable long id){
+        return funcionarioService.findById(id);
+    }
+
+    @GetMapping("list")
+    public List<Funcionario> findAllFuncionarios(){
+        return funcionarioService.findAll();
+    }
+
+    @PutMapping
+    public Funcionario atualizarFuncionario(@RequestBody Funcionario funcionario, @RequestParam long id){
+        return funcionarioService.update(funcionario, id);
+    }
+
+    @DeleteMapping
+    public void deleteFuncionarioById(@RequestParam long id){
+        funcionarioService.deleteById(id);
+    }
+}
