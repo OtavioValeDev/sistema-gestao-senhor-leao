e9f8976aca1973795a3a262a184bacd604469ab5
Matheus Bonato <bqnato@gmail.com>
2025-12-06 10:27:19 -0300

    Criado Service do Funcionario

---
+++ src/main/java/com/example/projeto_test/funcionario/service/FuncionarioService.java
@@ -0,0 +1,49 @@
+package com.example.projeto_test.funcionario.service;
+
+import com.example.projeto_test.funcionario.model.Funcionario;
+import com.example.projeto_test.funcionario.repository.FuncionarioRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class FuncionarioService {
+
+    private final FuncionarioRepo funcionarioRepo;
+
+    public void create(Funcionario funcionario){
+        funcionarioRepo.save(funcionario);
+    }
+
+    public Funcionario findById(long id){
+        return funcionarioRepo.findById(id).orElseThrow(()
+                -> new RuntimeException("Employee Not Found"));
+    }
+
+    public List<Funcionario> findAll(){
+        return funcionarioRepo.findAll();
+    }
+
+    public Funcionario update(Funcionario funcionario, long id){
+        Funcionario funcionarioAntigo = findById(id);
+        Funcionario funcionarioAtualizado = Funcionario.builder()
+                .nome(funcionario.getNome() != null ?
+                        funcionario.getNome() : funcionarioAntigo.getNome())
+                .email(funcionario.getEmail() != null ?
+                        funcionario.getEmail() : funcionarioAntigo.getEmail())
+                .Turno(funcionario.getTurno() != null ?
+                        funcionario.getTurno() : funcionarioAntigo.getTurno())
+                .senha(funcionario.getSenha() != null ?
+                        funcionario.getSenha() : funcionarioAntigo.getSenha())
+                .id(funcionarioAntigo.getId())
+                .build();
+        return funcionarioRepo.save(funcionarioAtualizado);
+    }
+
+    public void deleteById(long id){
+        funcionarioRepo.deleteById(id);
+    }
+
+}
